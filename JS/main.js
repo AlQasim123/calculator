d10ed2f59fba323c6923b5e83b9628da59864036
Islam <eslambnalham@gmail.com>
2025-07-08 17:31:21 +0300

    Add the buttons functionality

--- JS/main.js
+++ JS/main.js
@@ -33,3 +33,61 @@ function operate(numOne, op, numTwo) {
             return "Math Error"
     }
 }
+
+// vars to contains the result of multi operation 
+// and the current number on the screen and the operator and
+// after equal button clicked to clear screen 
+let result, current, operator, done = false; 
+
+//
+const display = document.querySelector(".display");
+
+// select all digits element 
+const numbers = document.querySelectorAll(".num");
+numbers.forEach((num) => {
+    num.addEventListener("click", () => {
+        if (done) {
+            display.textContent = "";
+            done = false
+        };
+        display.textContent += num.textContent;
+    })
+});
+
+// select all operators element
+const operators = document.querySelectorAll(".op");
+operators.forEach((op) => {
+    op.addEventListener("click", () => {
+        if (display.textContent) {
+            if (operator) {
+                current = display.textContent
+                result = operate(result, operator, current) 
+            } else {
+                result = display.textContent;
+            }
+            operator = op.textContent
+            display.textContent = "";
+        };
+    })
+});
+
+// the equal button
+const equal = document.querySelector(".equal")
+equal.addEventListener("click", () => {
+    if (display.textContent && operator) {
+        current = display.textContent;
+        result = operate(result, operator, current)
+    } else  {
+        result = display.textContent
+    };
+    display.textContent = result
+    result = current = operator = undefined;
+    done = true;
+})
+
+// the clear button
+const clear = document.querySelector(".clear")
+clear.addEventListener("click", () => {
+    firstNum = secondNum = operator = undefined;
+    display.textContent = "";
+})
